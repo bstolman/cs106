af8c45de54460879831619defff05cbd771104c7
Ben Stolman <bstolman@skidmore.edu>
2014-09-17 11:15:06 -0400

    New Calc Scanner

---
+++ Calculator/src/Drangus.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author BenStolman
+ *
+ */
+public class Drangus {
+	public static void writeSpaces(int number) {
+		for (int i = 1; i <= number; i ++) {
+			System.out.print("");
+		
+		}
+	}
+}
