165679271babaa05a124e516dc4333cf717ff395
Ben Stolman <bstolman@skidmore.edu>
2014-09-17 13:44:34 -0400

    New CALC

--- Calculator/src/Stringtttt.java
+++ Calculator/src/Stringtttt.java
@@ -10,12 +10,14 @@ System.out.println("Total Meal Amount");
 System.out.print("$");
 double total = in.nextDouble();
 System.out.println("How much would you like to tip?");
-for (double i1 = 1; i1 <=5; i1++) {
-	System.out.println(i1);}
+
+for (double i1 = 15; i1 <=20; i1++) {
+	System.out.println("%" + i1 + "?");}
 double tip = in.nextDouble(); {
-	System.out.print("$" + total * tip);
-	double check = total * tip;
-	
+	System.out.println("$" + total * tip/100);
+System.out.println("Your total is...");
+System.out.println("$" + ((total * tip/100) + total));	
+System.out.print("Here is a mint");
 	
 	
 		}
