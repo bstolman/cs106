3c52297a858cfbcab55cf609428eca7261435031
Ben Stolman <bstolman@skidmore.edu>
2014-10-31 10:48:06 -0400

    Project 5 Updated

--- Project5/src/second.java
+++ Project5/src/second.java
@@ -1,10 +1,11 @@
+import java.io.FileNotFoundException;
 
 public class second {
-public static void main(String[] args) {
+public static void main(String[] args) throws FileNotFoundException {
 	  HipHipHorrayforArray fileReader = new HipHipHorrayforArray();
-	  fileReader.openFile();
 	  fileReader.readFile();
 	  fileReader.closeFile();
 	  
 	}
 }
+
